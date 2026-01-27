2d80acb3dd05b218d14d6802ed38c19c65c735f9
jollagit <giuliaviga@gmail.com>
2026-01-27 16:59:23 +0100

    some work done for wordpress service

--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -20,28 +20,39 @@ if [ ! -f wp-config.php ]; then
 	wp core download --allow-root
 
 	DB_PWD=$(cat /run/secrets/db_wordpress_password)
-	WORDPRESS_DB_PWD=(cat /run/secrets/db_wordpress_password)
 	ADMIN_PWD=$(cat /run/secrets/wp_admin_password)
 	USER2_PWD=$(cat /run/secrets/wp_user2_password)
 
 # COMANDI WP-CLI che configurano WordPress in FASI:
 
+# Crea i file di CONFIGURAZIONE
+# Crea il file wp-config.php ( FILE DI CONFIG. PRINCIPALE DI WORDPRESS )
+# Stabilisce la connessione con il DATABASE (Nome DB, Utente, Password, Host)
 	wp config create \
 		--dbname="$WORDPRESS_DB_NAME" \
 		--dbuser="$WORDPRESS_DB_USER" \
-		--dbpass="$WORDPRESS_DB_PWD" \
+		--dbpass="$DB_PWD" \
 		--dbhost="$WORDPRESS_DB_HOST" \
 		--allow-root
 
 # Installa wordpress
+# Crea le TABELLE nel DATABASE (wp_posts, wp_users ecc... ) ??
+# CONFIGURA il sito: URL, titolo
+# CREA il primo Utente amministratore con CREDENZIALI specificate
+# Rende WordPress EFFETTIVAMENTE FUNZIONANTE
+
+# Dopo Questo comando il sito è ACCESSIBILE e OPERATIVO
 	wp core install \
 		--url="https://$DOMAIN_NAME" \
-		# --title="My Site" \ ???
+		--title="My Site" \
 		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$ADMIN_PWD" \
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--allow-root
 
+# Aggiunge Utenti EXTRA 
+# Crea un secondo utente con RUOLO "Author" ( puo' scrivere articoli ma non MODIFICARE Impostazioni )
+# Si esegue DOPO l'installazione perche si richiede che il Database WORDPRESS esista GIÀ
 	wp user create "$WP_USER2" "$WP_USER2_EMAIL" \
 		--role=author \
 		--user_password="$USER2_PWD" \
@@ -50,3 +61,6 @@ if [ ! -f wp-config.php ]; then
 fi
 
 exec "$@"
+
+# exec --> SOSTITUISCE il processo correte (bash) con un NUOVO PROCESSO: Quello passato come argomento
+# RIMPIAZZA bash con il processo di CMD php-fpm 
